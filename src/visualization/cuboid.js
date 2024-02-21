deda7efedc2e0b5f97cea691a17f762365512da9
Sören Möller <soeren-moeller99@gmx.de>
2024-02-21 15:43:28 +0100

    added basic drawing with new method

--- src/visualization/cuboid.js
+++ src/visualization/cuboid.js
@@ -3,11 +3,93 @@ import * as SVG from "./svg.js";
 import * as C from "../constants.js";
 import * as typedef from "../typedefs.js";
 
-export function drawScheme(scene, scheme, opacity) {
-    const sts = scheme.statements.filter((e) => {return e.width() != 0 && e.depth() != 0});
+function drawCuboid(scene, sts, baseGeometry, opacity) {
+    const materialBox = new THREE.MeshBasicMaterial( {color: C.WHITE, opacity: opacity, transparent: true} ); 
+    const mesh = new THREE.InstancedMesh(baseGeometry, materialBox, sts.length);
+
+    const dummy = new THREE.Object3D();
+    for (let i = 0; i < mesh.count; i++) {
+        const st = sts[i];
+        dummy.position.set(st.centerX(), st.centerY(), st.centerZ());
+        dummy.updateMatrix();
+        mesh.setMatrixAt( i, dummy.matrix );
+    }
+    mesh.instanceMatrix.needsUpdate = true;
+    scene.add(mesh);
+}
+
+function drawCuboidEdges(scene, sts, baseGeometry) {
+    if (sts.length == 0) {
+        return;
+    }
     console.log(sts);
-    let baseGeom = new THREE.EdgesGeometry(new THREE.BoxGeometry(1, 1, 1))
-    let instancedGeom = new THREE.InstancedBufferGeometry().copy(baseGeom)
+    const edgesGeom = new THREE.EdgesGeometry(baseGeometry);
+    const instancedGeom = new THREE.InstancedBufferGeometry().copy(edgesGeom);
+    instancedGeom.instanceCount = sts.length;
+
+    const instPos = [];
+    const instScale =[];
+    for (let i = 0; i < sts.length; i++) {
+        const st = sts[i];
+        instPos.push(st.centerX(), st.centerY(), st.centerZ());
+        instScale.push(st.width(), st.height(), st.depth());
+    }
+
+    instancedGeom.setAttribute(
+        "aScale",
+        new THREE.InstancedBufferAttribute(new Float32Array(instScale), 3, false)
+    );
+    instancedGeom.setAttribute(
+        "aPosition",
+        new THREE.InstancedBufferAttribute(new Float32Array(instPos), 3, false)
+    );
+
+    const instMat = new THREE.LineBasicMaterial({
+        color: 0x000000, 
+        onBeforeCompile: shader => {
+            shader.vertexShader = document.getElementById('vertex-shader-scale').textContent;
+        }
+    });
+    
+    const instEdges = new THREE.LineSegments(instancedGeom, instMat);
+    scene.add(instEdges);
+}
+
+export function drawScheme(scene, scheme, opacity) {
+    // I rescale a box with width=height=depth=1. Since unit intervals exist,
+    // I have to make a distinction if an axis is a unit interval.
+    const sts = scheme.statements;
+    const cuboidSts = sts.filter((e) => {return e.width() != 0 && e.depth() != 0 && e.height != 0});
+    const cuboidStsHeight = sts.filter((e) => {return e.width() != 0 && e.depth() != 0 && e.height == 0});
+    const xUnitSts = sts.filter((e) => {return e.width() == 0 && e.depth() != 0 && e.height != 0});
+    const xUnitStsHeight = sts.filter((e) => {return e.width() == 0 && e.depth() != 0 && e.height == 0});
+    const yUnitSts = sts.filter((e) => {return e.width() != 0 && e.depth() == 0 && e.height != 0});
+    const yUnitStsHeight = sts.filter((e) => {return e.width() != 0 && e.depth() == 0 && e.height == 0});
+    const dotSts = sts.filter((e) => {return e.width() == 0 && e.depth() == 0 && e.height != 0});
+    const dotStsHeight = sts.filter((e) => {return e.width() == 0 && e.depth() == 0 && e.height == 0});
+
+    drawCuboidEdges(scene, cuboidSts, new THREE.BoxGeometry(1, 1, 1));
+    drawCuboidEdges(scene, cuboidStsHeight, new THREE.BoxGeometry(1, 0, 1));
+    drawCuboidEdges(scene, xUnitSts, new THREE.BoxGeometry(0, 1, 1));
+    drawCuboidEdges(scene, xUnitStsHeight, new THREE.BoxGeometry(0, 0, 1));
+    drawCuboidEdges(scene, yUnitSts, new THREE.BoxGeometry(1, 1, 0));
+    drawCuboidEdges(scene, yUnitStsHeight, new THREE.BoxGeometry(1, 0, 0));
+    drawCuboidEdges(scene, dotSts, new THREE.BoxGeometry(0, 1, 0));
+    drawCuboidEdges(scene, dotStsHeight, new THREE.BoxGeometry(0, 0, 0));
+    drawCuboid(scene, cuboidSts, new THREE.BoxGeometry(1, 1, 1), opacity);
+    drawCuboid(scene, cuboidStsHeight, new THREE.BoxGeometry(1, 0, 1), opacity);
+    drawCuboid(scene, xUnitSts, new THREE.BoxGeometry(0, 1, 1), opacity);
+    drawCuboid(scene, xUnitStsHeight, new THREE.BoxGeometry(0, 0, 1), opacity);
+    drawCuboid(scene, yUnitSts, new THREE.BoxGeometry(1, 1, 0), opacity);
+    drawCuboid(scene, yUnitStsHeight, new THREE.BoxGeometry(1, 0, 0), opacity);
+    drawCuboid(scene, dotSts, new THREE.BoxGeometry(0, 1, 0), opacity);
+    drawCuboid(scene, dotStsHeight, new THREE.BoxGeometry(0, 0, 0), opacity);
+
+    //SVG.drawCuboidQualities(sts, cuboidSts);
+    
+    return;
+    const baseGeom = new THREE.EdgesGeometry(new THREE.BoxGeometry(1, 1, 1))
+    const instancedGeom = new THREE.InstancedBufferGeometry().copy(baseGeom)
     instancedGeom.instanceCount = sts.length;
 
     const instPos = [];
@@ -170,7 +252,7 @@ export function drawScheme2(scene, scheme, opacity) {
  * @param {THREE.Scene} scene 
  * @param {typedef.Statement} st 
  */
-function drawCuboid(scene, st, opacity) {
+function drawCuboid2(scene, st, opacity) {
     if (st.width() !== 0 || st.depth() !== 0) {
         if (JSON.stringify(st.y) == JSON.stringify([-Infinity, Infinity])) {
             const geometry = new THREE.BoxGeometry( 
